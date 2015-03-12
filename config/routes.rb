d7a09213a848ac11708415e5b5c9bb70e77c3161
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-03-11 23:10:57 -0700

    I'M DONE

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
+  get "dogs", to: "dogs#index"
+  get "/dogs/new", to: "dogs#new"
+  get "dogs/:id", to: "dogs#show", as: :dog
   post "users", to: "users#create"
+  post "dogs", to: "dogs#create"
 end
